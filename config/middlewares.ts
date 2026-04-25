5d15e951e66b3ed26c035252796b42f2cb1723f8
Cleydson Souza <cleydson.e.s.luz@gmail.com>
2026-04-25 03:08:31 -0300

    fix: update cors

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -24,6 +24,7 @@ const config: Core.Config.Middlewares = [
       origin: [
         'http://localhost:5173', // Vite dev server (padrão)
         'http://localhost:3000', // fallback CRA / outro
+        'https://behend.com.br',
         'https://bescore.vercel.app',
         process.env.FRONTEND_URL ?? '',
       ].filter(Boolean),
