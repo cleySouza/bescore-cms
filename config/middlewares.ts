0c3f04f7e2e98a20b1603f6a7c225b6ec694274c
Cleydson Souza <cleydson.e.s.luz@gmail.com>
2026-04-25 03:17:37 -0300

    fix: update cors

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -23,8 +23,8 @@ const config: Core.Config.Middlewares = [
       // Adicione aqui a URL do seu frontend em produção
       origin: [
         'http://localhost:5173', // Vite dev server (padrão)
-        'http://localhost:3000', // fallback CRA / outro
         'https://behend.com.br',
+        'https://www.behend.com.br',
         'https://bescore.vercel.app',
         process.env.FRONTEND_URL ?? '',
       ].filter(Boolean),
